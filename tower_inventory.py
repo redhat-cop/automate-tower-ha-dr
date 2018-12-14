286c5c404f69e762649238303ba7e282d967a962
Marc Petrivelli <mpetrive@redhat.com>
2018-12-14 17:30:21 -0500

    failover updates

--- tower_inventory.py
+++ tower_inventory.py
@@ -41,30 +41,35 @@ if __name__ == '__main__':
 
     args = parser.parse_args()
 
-    if os.path.exists("vars_tower.yml"):
-        with open("vars_tower.yml") as tv:
+    inv_path = os.path.dirname(os.path.abspath(__file__))
+    vars_tower_file = os.path.join(inv_path,"vars_tower.yml")
+    if os.path.exists(vars_tower_file):
+        with open(vars_tower_file) as tv:
             tower_inventory = yaml.load(tv.read())
     else:
-        raise Exception("vars_tower.yml must be present & well-formed with inventory names")
+        raise Exception(vars_tower_file + " must be present & well-formed with inventory names")
 
-    if not os.path.exists(tower_inventory.get("tower_inventory_pm", "EOF")):
+    inv_pm_file = os.path.join(inv_path, tower_inventory.get("tower_inventory_pm"))
+    if not os.path.exists(inv_pm_file):
+        raise Exception(vars_tower_file + " must contain a tower_inventory_pm file")
 
-        raise Exception("vars_tower.yml must contain a tower_inventory_pm file")
+    inv_ha_file = os.path.join(inv_path, tower_inventory.get("tower_inventory_ha"))
+    inv_dr_file = os.path.join(inv_path, tower_inventory.get("tower_inventory_dr"))
+    if not os.path.exists(inv_ha_file) and not os.path.exists(inv_dr_file):
+        raise Exception(vars_tower_file + " must contain a tower_inventory_dr or tower_inventory_ha file")
 
-    if not os.path.exists(tower_inventory.get("tower_inventory_dr", "EOF")) and \
-       not os.path.exists(tower_inventory.get("tower_inventory_ha", "EOF")):
-
-        raise Exception("vars_tower.yml must contain a tower_inventory_dr or tower_inventory_ha file")
-
-    if not os.path.exists('.tower_inventory.ini'):
+    inv_ini_path = os.path.join(inv_path, '.tower_inventory.ini')
+    #default to primary
+    if not os.path.exists(inv_ini_path):
         if tower_inventory:
-            inventory_file = tower_inventory.get("tower_inventory_pm", None)
+            inventory_file = inv_pm_file #tower_inventory.get("tower_inventory_pm", None)
     else:
-        with open('.tower_inventory.ini') as fh:
+        with open(inv_ini_path) as fh:
             inventory_file = fh.read()
             inventory_file = inventory_file.strip()
+            inventory_file = os.path.join(inv_path, inventory_file)
 
-    if inventory_file not in [ v for k, v in tower_inventory.iteritems() if k.startswith("tower_inventory_") ]:
-        raise Exception(".tower_inventory.ini does not contain a valid inventory.  Found " + inventory_file)
+    #if inventory_file not in [ v for k, v in tower_inventory.iteritems() if k.startswith("tower_inventory_") ]:
+    #    raise Exception(".tower_inventory.ini does not contain a valid inventory.  Found " + inventory_file)
 
     print _get_inventory(inventory_file)
